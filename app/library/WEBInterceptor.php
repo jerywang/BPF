196107a07bbf2f97b8c25014e9b492e26ac19a69
wangguoxing <wangguoxing@baidu.com>
2016-03-29 14:19:51 +0800

    add app cost, memory use log

--- app/library/WEBInterceptor.php
+++ app/library/WEBInterceptor.php
@@ -9,10 +9,9 @@ class WEBInterceptor extends Interceptor {
     private $memory = null;
 
     public function before() {
-        $this->time = -microtime(true);
-        $this->memory = -memory_get_usage();
-
         if ($this->is_enable_debug()) {
+            $this->time = -microtime(true);
+            $this->memory = -memory_get_usage();
             xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
         }
     }
@@ -32,15 +31,15 @@ class WEBInterceptor extends Interceptor {
     }
 
     public function after() {
-        $this->time += microtime(true);
-        $this->memory += memory_get_usage();
-        echo '
-            <div style="margin-top:50px;padding:10px;border-top:solid 1px #ccc">
-            <div>此次运行消耗内存：' . ($this->memory / 1024) . 'K</div>
-            <div>此次运行消耗时间：' . ($this->time * 1000) . 'ms</div>
-            </div>
-        ';
         if ($this->is_enable_debug()) {
+            $this->time += microtime(true);
+            $this->memory += memory_get_usage();
+            echo '
+                <div style="margin-top:50px;padding:10px;border-top:solid 1px #ccc">
+                <div>此次运行消耗内存：' . ($this->memory / 1024) . 'K</div>
+                <div>此次运行消耗时间：' . ($this->time * 1000) . 'ms</div>
+                </div>
+            ';
             $data = xhprof_disable();
             // xhprof_lib在下载的包里存在这个目录,记得将目录包含到运行的php代码中
             include_once APP_PATH . "xhprof/xhprof_lib/utils/xhprof_lib.php";
@@ -50,11 +49,10 @@ class WEBInterceptor extends Interceptor {
             // 第二个参数是自定义的命名空间字符串(任意字符串),
             // 返回运行ID,用这个ID查看相关的运行结果
             $run_id = $objXhprofRun->save_run($data, "xhprof");
-            echo $run_id;
             echo "---------------\n" .
                 "Assuming you have set up the http based UI for \n" .
                 "XHProf at some address, you can view run at \n" .
-                "<a target='_blank' href='http://dev.jerry.com/xhprof/xhprof_html/?run=$run_id&source=xhprof' style='color:#ff0000;font-weight:bolder'>查看性能</a>\n" .
+                "<a target='_blank' href='http://dev.jerry.com/xhprof/xhprof_html/index.php?run=$run_id&source=xhprof' style='color:#ff0000;font-weight:bolder'>查看性能</a>\n" .
                 "---------------\n";
         }
     }
